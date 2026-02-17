57ecdc94329641badd722f6898381559e41ea461
Daniel <viizion66@gmail.com>
2026-02-17 12:24:55 -0800

    Deleted svg files and testing JS date time functions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,10 @@
 const Sidebar = () => {
-    return ( <div className="sidebar">hello</div> );
+    return (
+        <div className="sidebar">
+            <h2>Atlas Beta</h2>
+            <h4>{Date.now()}</h4>
+        </div>
+    );
 }
- 
+
 export default Sidebar;
