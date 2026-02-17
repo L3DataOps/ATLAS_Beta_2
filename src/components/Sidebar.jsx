9b63039e6d253ea6fac894702240640bf16081e4
Daniel <viizion66@gmail.com>
2026-02-17 12:41:29 -0800

    Testing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
 const Sidebar = () => {
+
+    let currentDate = new Date(timestamp)
+
     return (
         <div className="sidebar">
             <h2>Atlas Beta</h2>
-            <h4>{Date.now()}</h4>
+            <h4>{currentDate.toLocaleString()}</h4>
         </div>
     );
 }
